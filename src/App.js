95914bfe200610957c0f70aae89b14c898ab486c
AlexFP <florianprescott@googlemail.com>
2018-05-15 17:46:52 -0400

    redo framework

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Col, Container, Row } from "reactstrap";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Container className="wrapper">
+        <Row>
+        </Row>
+        <Row>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
